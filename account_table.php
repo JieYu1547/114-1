b1935287b1894d406a789051f5d0a7fcfac29af2
JieYu1547 <yulin1081229@gmail.com>
2025-04-09 11:57:32 +0800

    account_table

---
+++ account_table.php
@@ -0,0 +1,11 @@
+﻿<table border=1>
+<tr><th>ID</th><th>name</th></tr>
+<?php
+$mysqli = new mysqli("localhost", "root", "", "msgboard");
+$result = $mysqli->query("SELECT * FROM account");
+$rows = $result->fetch_all(MYSQL_ASSOC);
+foreach($rows as $row){
+	printf("<tr><td>%d</td><td>%s</td></tr>\n",$row["idno"],$row["name"]);
+}
+?>
+</table>
