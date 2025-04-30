017172b43620c73615150992ec0bd144e1033896
JieYu1547 <yulin1081229@gmail.com>
2025-04-30 12:02:10 +0800

    test

--- account_table.php
+++ account_table.php
@@ -3,7 +3,7 @@
 <?php
 $mysqli = new mysqli("localhost", "root", "", "msgboard");
 $result = $mysqli->query("SELECT * FROM account");
-$rows = $result->fetch_all(MYSQL_ASSOC);
+$rows = $result->fetch_all(MYSQLI_ASSOC);
 foreach($rows as $row){
 	printf("<tr><td>%d</td><td>%s</td></tr>\n",$row["idno"],$row["name"]);
 }
